2c56e86085b34a66e45bbdacc1d7b986f0852a56
Qi Chen <chenqish@cn.ibm.com>
2015-02-03 12:36:28 +0800

    run through Karma & Jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'test/unit/jasmine.js'
     ],
 
 
@@ -54,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
---
+++ test/unit/jasmine.js
@@ -0,0 +1,7 @@
+'use strict'
+
+describe('init Jasmine test', function() {
+	it("contains spec with an expectation", function() {
+		expect(true).toBe(true);
+	}); 
+});
